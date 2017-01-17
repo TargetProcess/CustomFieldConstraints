32f29103e8c4237379bf82afd1bb8020f642c141
Dmitry Tsarevich <tsarevich@targetprocess.com>
2017-01-17 12:36:22 +0300

    Rename production config to manager config
    Production option is controlled explicitly, so rename config to suggest
    it is related to manager, not to production.

--- scripts/build.manager.js
+++ scripts/build.manager.js
@@ -1,6 +1,6 @@
 const builder = require('./builder');
 
 const buildPath = 'build/manager';
-const managerConfig = 'webpack-production.config.js';
+const managerConfig = 'webpack-manager.config.js';
 
 builder.build(buildPath, managerConfig);
